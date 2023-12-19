b2915ad087bba661b371ed5034c13663b2c5f8c3
mspsohan <mspsohan@gmail.com>
2023-12-19 14:08:00 +0600

    Added Welcome Screen When Site First Render

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,38 +1,17 @@
-import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar/Navbar";
-import Footer from "../components/Footer/Footer";
-import Loading from "../components/Loading/Loading";
-import { useEffect, useState } from "react";
-const LoadingSpinner = () => {
-   return (
-      <div className="">
-         <Loading />
-      </div>
-   );
-};
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar/Navbar';
+import Footer from '../components/Footer/Footer';
+
 const Main = () => {
-   const [isLoading, setIsLoading] = useState(true);
-   useEffect(() => {
-      const fetchData = async () => {
-         await new Promise(resolve => setTimeout(resolve, 5000));
-         setIsLoading(false);
-      };
-
-      fetchData();
-   }, []);
-   return (
-      <>
-         {isLoading && <LoadingSpinner />}
-         {!isLoading && <>
-            <Navbar />
-            <div className="">
-               <Outlet />
-            </div>
-            <Footer />
-         </>}
-
-      </>
-   );
+	return (
+		<>
+			<Navbar />
+			<div className=''>
+				<Outlet />
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Main;
